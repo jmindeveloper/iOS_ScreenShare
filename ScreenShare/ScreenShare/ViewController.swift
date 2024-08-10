d000d3bd609e48436bd2f9e3d67f226e562ff4c5
jmindeveloper <jmin.developer@gmail.com>
2024-08-10 21:24:58 +0900

    screen share button 추가

--- ScreenShare/ScreenShare/ViewController.swift
+++ ScreenShare/ScreenShare/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import ReplayKit
 
 class ViewController: UIViewController {
 
+    private let startScreenShareButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
+        button.contentVerticalAlignment = .fill
+        button.contentHorizontalAlignment = .fill
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setSubViews()
     }
 
+    private func setSubViews() {
+        view.addSubview(startScreenShareButton)
+        
+        setConstraints()
+        connectTarget()
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            startScreenShareButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            startScreenShareButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            startScreenShareButton.widthAnchor.constraint(equalToConstant: 60),
+            startScreenShareButton.heightAnchor.constraint(equalToConstant: 60),
+        ])
+    }
+    
+    private func connectTarget() {
+        startScreenShareButton.addTarget(self, action: #selector(startScreenShareButtonAction), for: .touchUpInside)
+    }
+    
+    @objc private func startScreenShareButtonAction() {
+        print("stsart screen share")
+    }
 
 }
 
